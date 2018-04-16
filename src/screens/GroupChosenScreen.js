1c25e2e9035e5ebe0fa609383045b5df9915268f
Chi Yu <cfyu@princeton.edu>
2018-04-15 21:28:14 -0400

    shows correct time for group without one person 30 min

--- src/screens/GroupChosenScreen.js
+++ src/screens/GroupChosenScreen.js
@@ -1,3 +1,5 @@
+// Note: this page is in need of reformatting because right now the code is very ugly.
+
 import React from 'react';
 import { View, Image, Text, TouchableHighlight, SectionList, StyleSheet, ActivityIndicator } from 'react-native';
 import NavigationBar from 'navigationbar-react-native';
@@ -10,6 +12,10 @@ const db = firebase.firestore();
 const numdays = [31,28,31,30,31,30,31,31,30,31,30,31];
 const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
 const days = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const today = new Date();
+const todayMonth = today.getMonth();
+const todayDate = today.getDate();
+const todayDay = today.getDay();
 
 export default class GroupChosenScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -26,16 +32,68 @@ export default class GroupChosenScreen extends React.Component {
     const { params } = this.props.navigation.state;
     await this.matchGroup(params.members);
   }
+
   async matchGroup(members) {
     freeTimeObj = new Object();
     for (let id in members) {
       freeTimeObj[id] = await this.getFreeTimes(id);
-      console.log("HERE 1")
+      // console.log("HERE 1")
     }
-    console.log("HERE 2")
+    // console.log("HERE 2")
     matches1 = this.match30min(freeTimeObj);
     matches2 = this.match1hr(freeTimeObj);
-    this.setState({matches1:matches1,matches2:matches2})
+    noMatches1 = this.checkNoMatches(matches1);
+    noMatches2 = this.checkNoMatches(matches2);
+
+    freeTimeWithoutOne = [];
+    // If there are no matches...
+    if (noMatches1) {
+      for (friendID in members) {
+        // does not exclude the user itself
+        if (friendID !== userID) {
+          let newMembers = Object.assign({},members);
+          delete newMembers[friendID];
+
+          //console.log("members:", members)
+          console.log("newMebers:", newMembers)
+
+          newFreeTimeObj = new Object();
+          for (let id in newMembers) {
+            newFreeTimeObj[id] = await this.getFreeTimes(id);
+            // console.log("I'm here 1; id =", id)
+            // console.log(members[id])
+          }
+          newMatches1 = this.match30min(newFreeTimeObj);
+          // console.log(newMatches1)
+          console.log(members[friendID], newMatches1)
+          noNewMatches1 = this.checkNoMatches(newMatches1);
+          console.log(noNewMatches1)
+
+          if (!noNewMatches1) {
+            // console.log("Hey", newMatches1)
+            temp = [];
+            for (thisday in newMatches1) {
+              // console.log("thisday", thisday)
+              for (j = 0; j < 25; j++) {
+                if (newMatches1[thisday][j]) {
+                  console.log("day", thisday)
+                  diff = days.indexOf(thisday) - todayDay;
+                  if (diff < 0) diff += 7;
+                  // console.log("day ", day, "diff ", diff, "date ", date)
+                  temp.push(this.printDate(todayMonth,todayDate,todayDay,diff) + ": " + this.printTime(j) + "-" + this.printTime(j+1,true))
+                }
+              }
+            }
+            console.log(temp)
+            freeTimeWithoutOne.push({title: "rip " + members[friendID], data: temp})
+          }
+          // console.log("I'm here 2; friendID =", friendID)
+        }
+        // console.log("I'm here 3; friendID =", friendID)
+    }
+  }
+
+    this.setState({matches1:matches1,matches2:matches2,noMatches1:noMatches1,noMatches2:noMatches2,freeTimeWithoutOne:freeTimeWithoutOne})
   }
 
   updateIndex = (index) => {
@@ -62,6 +120,16 @@ export default class GroupChosenScreen extends React.Component {
     if (hour > 12) hour -= 12
     if (!ampm) return hour + ":" + min
     else return hour + ":" + min + " " + time
+  }
+
+  checkNoMatches = (matches) => {
+    for (day in matches) {
+      for (i = 0; i < matches[day].length; i++) {
+        if (matches[day][i])
+          return false
+      }
+    }
+    return true
   }
 
   getFreeTimes = (id) => {
@@ -92,10 +160,11 @@ export default class GroupChosenScreen extends React.Component {
           matches[day][i] = false
           continue;
         }
-        for (friend in freeTimeObj)
+        for (friend in freeTimeObj) {
           if (freeTimeObj[friend][day][i] != 1) {
             matches[day][i] = false
           }
+        }
       }
     }
     return matches;
@@ -135,9 +204,112 @@ export default class GroupChosenScreen extends React.Component {
     const members = params.members
 
     if (this.state.matches1) {
+      console.log("Re-render")
       const reschedule = params ? params.reschedule : undefined;
       const sent = params ? params.sent : undefined;
 
+      if (this.state.noMatches1) {
+        console.log(this.state.freeTimeWithoutOne)
+        return(
+          <View style={{flex:1}}>
+            <View style={{alignItems:'center'}}>
+              <View style={{flexDirection:'row', overflow: 'hidden', paddingRight:10, borderRadius:50}} >
+                  <View style={{overflow: 'hidden', borderTopLeftRadius: 50, borderBottomLeftRadius: 50}}>
+                    <Image
+                      style={{width: 50, height: 100,}}
+                      source={{uri:urls[0]}} />
+                  </View>
+                  <View style ={{overflow: 'hidden', borderTopRightRadius: 50, borderBottomRightRadius: 50}}>
+                    <Image
+                      style={{width: 50, height: 50, }}
+                      source={{uri:urls[1]}} />
+                    <Image
+                      style={{width: 50, height: 50, }}
+                      source={{uri:urls[2]}}/>
+                  </View>
+                </View>
+            <Text style={{fontSize:15}}>{'Looks like yall pretty busy, what about...'}</Text>
+            </View>
+            <ScrollableTabView
+              style={{marginTop: 0, flex:1}}
+              renderTabBar={() => <DefaultTabBar />}
+              onChangeTab = {()=>{}}
+              tabBarBackgroundColor = {'white'}
+              tabBarActiveTextColor = {'black'}
+              tabBarInactiveTextColor = {'black'}
+              tabBarUnderlineStyle = {{backgroundColor:'#f4511e'}}
+            >
+              <SectionList
+                tabLabel='30 min'
+                sections={this.state.freeTimeWithoutOne}
+                renderItem={({item,section}) =>
+                  <ListItem
+                title={item}
+                onPress={() => {
+                  // t = section.title.split(", ");
+                  month = 1;
+                  date = 1;
+                  // time = item.split("-")
+                  hour = 1
+                  min = 1
+                  // if (item.slice(-2) == "pm" && hour != 12 && time[0] != "11:30") hour += 12
+                  // Year is hardcoded as 2018
+                  ymd = new Date(2018,month,date,hour,min)
+                  this.props.navigation.navigate('FinalRequest',
+                  {
+                    sent: sent,
+                    reschedule: reschedule,
+                    // name: name,
+                    // id: id,
+                    name: groupName,
+                    members: members,
+                    dateobj: ymd.toString(),
+                    time: item,
+                    length: 0.5,
+                    isGroup: true,
+                  })
+                }}
+              />}
+                renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+                keyExtractor={(item, index) => index}
+              />
+              <SectionList
+                tabLabel='1 hr (but right now its 30 min lol)'
+                sections={this.state.freeTimeWithoutOne}
+                renderItem={({item,section}) =>
+                  <ListItem
+                    title={item}
+                    onPress={() => {
+                      // t = section.title.split(", ");
+                      month = 1;
+                      date = 1;
+                      // time = item.split("-")
+                      hour = 1
+                      min = 1
+                      // if (item.slice(-2) == "pm" && hour != 12 && time[0] != "11:30") hour += 12
+                      // Year is hardcoded as 2018
+                      ymd = new Date(2018,month,date,hour,min)
+                      this.props.navigation.navigate('FinalRequest',
+                      {
+                        sent: sent,
+                        reschedule: reschedule,
+                        // name: name,
+                        // id: id,
+                        name: groupName,
+                        members: members,
+                        dateobj: ymd.toString(),
+                        time: item,
+                        length: 1,
+                      })
+                    }}
+                  />}
+                renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+                keyExtractor={(item, index) => index}
+              />
+            </ScrollableTabView>
+          </View>
+        )
+      }
       match1 = [];
       match2 = [];
       d = new Date();
@@ -184,11 +356,13 @@ export default class GroupChosenScreen extends React.Component {
       for (i of match2) {
         i.title = this.printDate(month,date,day,i.title)
       }
+ 
       urls = []
       for (memberID in members) {
         urls.push(`http://graph.facebook.com/${memberID}/picture?type=large`)
       }
       urls.push(`http://graph.facebook.com/${userID}/picture?type=large`)
+
       return(
         <View style={{flex:1}}>
           <View style={{alignItems:'center'}}>
