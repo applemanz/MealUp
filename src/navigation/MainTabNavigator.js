447a2586956143ea743a9b3c40b3e80a8bd8a97a
azou1103 <azou@princeton.edu>
2018-04-20 14:17:40 -0400

    still need to debug navigation...

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -178,19 +178,6 @@ const MealsStack = StackNavigator({
       tabBarVisible: false
     }
   },
-  FreeTime: {
-    screen: FreeTimeScreen,
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: '#f4511e',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-        fontSize: 25
-      },
-    },
-  },
   RequestByFriend: {
     screen: RequestByFriendScreen,
     navigationOptions: {
